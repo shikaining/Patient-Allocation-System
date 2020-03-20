482d0c6a788758e25ec03d79568858faa24f48c4
Cyrus Lee <46909543+cyrusdotes@users.noreply.github.com>
2020-03-20 23:39:21 +0800

    Add files via upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,12 +3,21 @@ const Patient = artifacts.require("Patient");
 module.exports = function(deployer, network, accounts) {
 	const owner = accounts[0];
 	const powerUsers = [accounts[1], accounts[2]];
+	const adminUsers = [accounts[3], accounts[4]];
 	let patientInstance;
 	
 	return deployer
     .then(() => {
-        return deployer.deploy(Patient, powerUsers, {from: owner});
+        return deployer.deploy(Patient, powerUsers, adminUsers, {from: owner});
     }).then((inst) => {
         patientInstance = inst;
-    })
+        //return deployer.deploy(ERC20, {from: owner});
+    })/*.then((_inst) => {
+        tokenInstance = _inst;
+        return deployer.deploy(CollectibleMarketPlace,
+                              patientInstance.address,
+                              tokenInstance.address,
+                              100,
+                              {from: platform});
+    });*/
 };
