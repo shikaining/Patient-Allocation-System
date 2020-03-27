ec2609022682cb3c7b5ca9ff124f9c31ddd37ad9
Shi Kai Ning <kai_ningg@hotmail.com>
2020-03-27 23:38:24 +0800

    edited allocatePatient to update ethereum & db

--- routes/allocatePatients.js
+++ routes/allocatePatients.js
@@ -30,32 +30,63 @@ router.get('/', function (req, res, next) {
             console.log("Patient" + data.rowCount);
             res.render('allocatePatients', { title: 'Allocate Patients', user: username, data: data.rows });
         });
+        //retrieve current staff object
+        var sql_query = "SELECT * FROM public.staff WHERE public.staff.email = $1";
+
+        pool.query(sql_query, [username], (err, data) => {
+            staff = data.rows[0];
+        });
     }
 });
 
 // POST
 router.post('/', function (req, res, next) {
     //retrieve information
-    var username = req.session.username;
     var patientId = req.body.patientId;
     var studId = req.body.studentId;
 
     console.log("Patient ID" + patientId);
+    console.log("Student ID" + studId);
+    try {
+        //Update into Ethereum
+        truffle_connect.allocatePatient(
+            patientId,
+            studentAddr,
+            staff.address
+        );
+        //update postgreSQL Database
+        //update allocatedStatus of patient
+        var allocatePatient = "UPDATE public.patient SET allocatedStatus = $1, studId = $2 WHERE pid = $3";
+        pool.query(allocatePatient, ['Allocated', studId, patientId], (err, data) => {
+            console.log(err);
+            if (err === undefined) {
+                req.flash('info', 'Patient Allocated');
+            } else {
+                req.flash('error', 'An error has occurred! Please try again');
+            }
 
-    //Update into Ethereum
-    //
-    //update postgreSQL Database
-    var allocatePatient = "UPDATE public.patient SET allocatedStatus = $1, studId = $2 WHERE pid = $3";
-    pool.query(allocatePatient, ['Allocated', studId, patientId], (err, data) => {
-        console.log(err);
-        if (err === undefined) {
-            req.flash('info', 'Patient Allocated');
-        } else {
-            req.flash('error', 'An error has occurred! Please try again');
-        }
+            //res.redirect('/allocatePatients');
+        });
+        //update allocatedStatus of request
+        var updateRequest_query =
+            "UPDATE public.request SET allocatedStatus = $1, WHERE pid = $2 AND studId = $3";
+        pool.query(
+            updateRequest_query,
+            ['Allocated', patientId, studId],
+            (err, data) => {
+                console.log(err);
+                if (err === undefined) {
+                    req.flash('info', 'Patient Allocated');
+                } else {
+                    req.flash('error', 'An error has occurred! Please try again');
+                }
 
-        res.redirect('/allocatePatients');
-    });
+                res.redirect('/allocatePatients');
+            });
+    } catch (error) {
+        console.log("ERROR at allocatePatient: " + error);
+        return;
+    }
 
 });
 
